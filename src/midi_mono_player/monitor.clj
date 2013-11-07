a17c6cc2daf36b27c43121bc3424e12f22b2c1fb
Bill Allen <bill@k2bea.org>
2013-11-07 12:20:38 -0500

    use check box/radio buttons instead of raw text for parameters

--- src/midi_mono_player/monitor.clj
+++ src/midi_mono_player/monitor.clj
@@ -1,6 +1,7 @@
 (ns midi-mono-player.monitor
   (:use [seesaw core style font]
-        [midi-mono-player player test-wx7])
+        [midi-mono-player player test-wx7]
+        [clojure.core.match :only (match)])
   (:require [overtone.libs.event :as e]))
 
 (native!)
@@ -25,14 +26,30 @@
     (grid-panel :rows 1 :columns (count items) :border "" :items items)))
 
 
+(defn make-radio-event-widget
+  [p]
+  (let [group (button-group)
+        info-label (label :text "" :border 10)
+        d (:default p)
+        panel (vertical-panel
+               :items (for [n (range (:min p) (+ (:max p) (:step p)) (:step p))]
+                        (radio :text (str n) :group group :selected? (= n d))))]
+    [panel group]))
+
+(defn make-discreet-event-widget
+  [p]
+  (match [(:min p) (:max p) (:step p)]
+         [0 1 1] [:checkbox (checkbox :selected? (= (:default p) 1)) nil]
+         :else (concat [:radio] (make-radio-event-widget p))))
+
 (defn make-switches-panel
   [events]
   (let [items (for [[n p] events]
-                (-> (grid-panel  :rows 2 :columns 1)
-                    (add! (label :text (str n ": " (:name p)) :class :event-title))
-                    (add! (let [t (text (str (:default p)))]
-                            (swap! mono-player-events assoc (:symbol p) {:type :discreet :widget t})
-                            t))))]
+                (border-panel
+                 :north (label :text (str n ": " (:name p)) :class :event-title)
+                 :center (let [[typ t g] (make-discreet-event-widget p)]
+                           (swap! mono-player-events assoc (:symbol p) {:type typ :widget t :group g})
+                           t)))]
     (grid-panel :rows 1 :columns (count items) :border "" :items items)))
 
 (defn make-main-panel
@@ -40,11 +57,7 @@
   ([cc-events pc-switches]
      (let [discreet-cc-events (filter #(= :discreet (:type (val %))) cc-events)
            continuous-cc-events (filter #(not= :discreet (:type (val %))) cc-events)]
-       (border-panel :north (-> (grid-panel :rows 3 :columns 1)
-                                (add! (make-switches-panel discreet-cc-events))
-                                (add! (if pc-switches
-                                        (make-switches-panel pc-switches)
-                                        (label ""))))
+       (border-panel :north (make-switches-panel discreet-cc-events)
                      :center (make-continuous-vals-panel continuous-cc-events)))))
 
 (defn make-exit
@@ -85,7 +98,12 @@
                 (when-let [e (get @mono-player-events (:type val))]
                   (case (:type e)
                     :continuous (config! (:widget e) :value  (* 100 (:val val)))
-                    :discreet (text! (:widget e) (str (:val val))))))
+                    :checkbox (config! (:widget e) :selected? (= val 1))
+                    :radio (doseq [b (config (:group e) :buttons)]
+                             (let [s (str (:val val))
+                                   t (text b)]
+                               (println s " " t)
+                               (config! b :selected? (= s t)))))))
               (concat event-key [:mono-midi-player-event]))
     (apply-stylesheet f {[:.event-title] {:font (font :style :bold)}})
     (invoke-later
